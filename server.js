ddca700698dd4067b342166de6e4965947e997c1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-28 17:53:13 +0100

    wip

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ const moodyGateKeeper = (req, res, next) => {
   // get seconds of Unix time `new Date().getSeconds()`
   // if the seconds multiple of 3, respond with a 403 and a "not allowed" message
   // otherwise call next to allow the request to proceed...
+  if (new Date().getSeconds() % 3 === 0) {
+    res.sta
+  }
 };
 
 // adding global middlewares with server.use
