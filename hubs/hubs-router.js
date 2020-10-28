14615ab92641657cbe963f9234a9feec6f17bff1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-28 18:35:06 +0100

    wip

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const validateId = (req, res, next) => {
   const { id } = req.params;
   Hubs.findById(id)
-    .then(data => {
+    .then(data => { // if id legit -> data is **object** (the hub) otherwise **undefined**
       console.log(data)
     })
     .catch(error => {
