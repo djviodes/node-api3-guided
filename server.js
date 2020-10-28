ee6cbf57a35fa56c18683f543524f2ca89c949c4
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-28 17:22:53 +0100

    wip

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = express();
 // adding global middlewares with server.use
 // the req and the res objects travel through them
 server.use(express.json()); // the req now has a body object
-server.use(helmet()) // the res now has better headers
+server.use(helmet()); // the res now has better headers
 
 // the router is a group of middlewares
 server.use('/api/hubs', hubsRouter);
